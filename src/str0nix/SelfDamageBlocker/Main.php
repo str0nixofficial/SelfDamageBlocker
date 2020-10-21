11c2921004abaf84f2de72cb3ca3fc8475437813
str0nixofficial <vargadomonkos2002@gmail.com>
2020-10-21 22:24:13 +0200

    fixed crashing issue

--- src/str0nix/SelfDamageBlocker/Main.php
+++ src/str0nix/SelfDamageBlocker/Main.php
@@ -7,8 +7,7 @@ namespace str0nix\SelfDamageBlocker;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageByChildEntityEvent;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\utils\Config;
@@ -29,12 +28,18 @@ class Main extends PluginBase implements Listener{
         }
     }
 
-    public function onEntityDamage(EntityDamageEvent $event) {
+    public function onSelfDamage(EntityDamageByChildEntityEvent $event) {
         $entity = $event->getEntity();
+        $cause = $event->getCause();
         $damager = $event->getDamager();
 
-        if($event instanceof EntityDamageByEntityEvent){
-            if($damager instanceof Player){
+        if($cause === EntityDamageByChildEntityEvent::CAUSE_PROJECTILE){
+            if($cause === EntityDamageByChildEntityEvent::CAUSE_FIRE && EntityDamageByChildEntityEvent::CAUSE_FIRE_TICK){
+                if($entity === $damager){
+                    $event->setCancelled();
+                    $entity->sendMessage($this->getConfig()->get("msg"));
+                }
+            } else {
                 if($entity === $damager){
                     $event->setCancelled();
                     $entity->sendMessage($this->getConfig()->get("msg"));
@@ -42,4 +47,21 @@ class Main extends PluginBase implements Listener{
             }
         }
     }
+
+    /*
+    public function onEntityDamage(EntityDamageEvent $event) {
+        $entity = $event->getEntity()->getName();
+        $damager = $event->getDamager()->getName();
+        $cause = $event->getCause();
+
+        if($event instanceof EntityDamageByEntityEvent){
+            if($event->getCause() != EntityDamageEvent::CAUSE_FIRE_TICK){
+                if($entity === $damager){
+                    $event->setCancelled();
+                    $entity->sendMessage($this->getConfig()->get("msg"));
+                }
+            }
+        }
+    }
+    */
 }
